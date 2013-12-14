13c9d0796392881a46e503f6037766d705f441b5
xun <447453902@qq.com>
2013-12-14 15:18:42 +0800

    init commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Axun::Application.config.secret_key_base = 'bba72446540889a23fc9c7fda7933e0b92fb602e1c21ca6ca68eb77a4dbb4ac41dc45d40fb08788a1215489baede40403706715bce67be36e9182d6b309a70f2'
